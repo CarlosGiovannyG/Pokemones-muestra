b99415b90291aa09b638ec62d8931527103fc887
Carlos Giovanny Gualtero <cggualtero@hotmail.com>
2021-12-04 00:23:58 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import store from '../src/Redux/Store'
 
 import reportWebVitals from './reportWebVitals';
 
-
-
 import './index.css';
 import Rout from './Routes/Routes';
-
-
 
 ReactDOM.render(
   <Provider store={store}>
